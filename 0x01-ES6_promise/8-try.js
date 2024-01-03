8271ff3a8e67e3ef39d7def792f3e4b3a2595929
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-03 16:03:41 +0100

    9th task is updated again

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) throw Error('annot divide by 0');
+  if (denominator === 0) {
+    throw new Error('Cannot divide by 0');
+  }
+
   return numerator / denominator;
 }
