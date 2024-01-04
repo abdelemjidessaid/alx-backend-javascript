0d6f9b19b9cbaff950ebcda8021f5fb7bc89b97e
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-04 15:12:56 +0100

    4th task is done

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') this._code = code;
+    else throw new TypeError('Code must be a string');
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+    else throw new TypeError('Name must be a string');
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
