bbe196887853a94547abe0172f77983b9e6d77e8
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-04 12:41:59 +0100

    2nd task is done

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classes = [];
+
+  classes.push(new ClassRoom(19));
+  classes.push(new ClassRoom(20));
+  classes.push(new ClassRoom(34));
+
+  return classes;
+}
