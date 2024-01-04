e1ada169888f8b26a86825448d4610e36892073c
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-04 14:57:34 +0100

    3rd task is done

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,10 +10,8 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if ((typeof name !== 'string') && !(name instanceof String)) {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
+    if (typeof name === 'string') this._name = name;
+    else throw new TypeError('Name must be a string');
   }
 
   get length() {
@@ -21,10 +19,8 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if ((typeof length !== 'number') && !(length instanceof Number)) {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = length;
+    if (typeof length === 'number') this._length = length;
+    else throw new TypeError('Length must be a number');
   }
 
   get students() {
@@ -32,9 +28,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = students;
+    if (Array.isArray(students)) {
+      if (students.every((item) => typeof item === 'string')) this._students = students;
+      else throw new TypeError('Students element must be a string');
+    } else throw new TypeError('Students must be an array');
   }
 }
