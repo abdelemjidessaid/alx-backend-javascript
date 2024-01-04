3fa6233ff781197a91e044393cfdd410bce63a71
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-04 16:19:23 +0100

    10th task is done

---
+++ 0x02-ES6_classes/9-main.js
@@ -0,0 +1,7 @@
+import { listOfStudents, HolbertonClass, StudentHolberton } from './9-hoisting';
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map((student) => student.fullStudentDescription);
+
+console.log(listPrinted);
