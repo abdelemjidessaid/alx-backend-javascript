7020a1b3b4d38b62ec4b7c6bd35d5d7dcb3b2338
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-09 12:35:08 +0100

    task 8 is updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0) return '';
+  if (
+    !startString || startString.length === 0 || !set
+    || !(set instanceof Set) || typeof startString !== 'string'
+  ) return '';
   const result = Array.from(set).reduce((acc, value) => {
     if (value.startsWith(startString)) {
       return acc + (acc.length === 0 ? '' : '-') + value.replace(startString, '');
