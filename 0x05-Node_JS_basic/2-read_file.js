229ceffd4b640c0eafba61a06ad968b57b6ad4c1
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-26 18:00:05 +0100

    3rd task is done

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
 
-module.exports = function countStudents(path) {
+function countStudents(path) {
   try {
-
     const data = fs.readFileSync(path, { encoding: 'utf-8' });
-
     const lines = data.split('\n').slice(1, -1);
-
     const header = data.split('\n').slice(0, 1)[0].split(',');
-
     const idxFn = header.findIndex((ele) => ele === 'firstname');
     const idxFd = header.findIndex((ele) => ele === 'field');
-
     const fields = {};
     const students = {};
 
@@ -34,3 +29,5 @@ module.exports = function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 };
+
+module.exports = countStudents;
