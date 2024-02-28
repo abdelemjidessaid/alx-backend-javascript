d2398942aad62fa718567249e206b4d8da050c68
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-28 16:11:12 +0100

    tasks 1 and 2 are updated

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -6,7 +6,7 @@ module.exports = function calculateNumber(type, a, b) {
   } else if (type === 'SUBTRACT') {
     return round(a) - round(b);
   } else if (type === 'DIVIDE') {
-    if (b === 0) {
+    if (round(b) === 0) {
       return 'Error';
     }
     return round(a) / round(b);
