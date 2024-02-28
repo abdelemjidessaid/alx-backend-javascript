583cd8f87ef77b7eb19a2c4be7161bb25ab0443d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-28 16:38:19 +0100

    The 3rd task is updated

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -3,11 +3,9 @@ const round = Math.round;
 module.exports = function calculateNumber(type, a, b) {
   if (type === 'SUM') {
     return round(a) + round(b);
-  }
-  if (type === 'SUBTRACT') {
+  } else if (type === 'SUBTRACT') {
     return round(a) - round(b);
-  }
-  if (type === 'DIVIDE') {
-    round(b) === 0 ? 'Error' : round(a) / round(b);
+  } else if (type === 'DIVIDE') {
+    return round(b) === 0 ? 'Error' : round(a) / round(b);
   }
 };
